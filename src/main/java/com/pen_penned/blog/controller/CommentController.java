3d32313d8971c57d0ea12e3e1fef45192d72d325
Franklyne-kibet <rootkibet@gmail.com>
2025-04-22 15:14:57 +0300

    refactor: optimize comment controller

--- src/main/java/com/pen_penned/blog/controller/CommentController.java
+++ src/main/java/com/pen_penned/blog/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.pen_penned.blog.controller;
 
+import com.pen_penned.blog.config.AppConstants;
 import com.pen_penned.blog.dto.request.CommentDTO;
+import com.pen_penned.blog.dto.response.CommentResponse;
 import com.pen_penned.blog.model.User;
 import com.pen_penned.blog.service.CommentService;
 import com.pen_penned.blog.util.AuthUtil;
@@ -31,6 +33,30 @@ public class CommentController {
     public ResponseEntity<CommentDTO> getCommentById(@PathVariable Long commentId) {
         CommentDTO commentDTO = commentService.getCommentById(commentId);
         return new ResponseEntity<>(commentDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/comments/posts/{postId}")
+    public ResponseEntity<CommentResponse> getPostComments(
+            @PathVariable Long postId,
+            @RequestParam(name = "pageNumber",
+                    defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize",
+                    defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy",
+                    defaultValue = AppConstants.SORT_COMMENTS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder",
+                    defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder) {
+        CommentResponse commentResponse = (CommentResponse) commentService.getCommentsByPost(postId,
+                pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(commentResponse, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/comments/{commentId}")
+    public ResponseEntity<CommentDTO> updateCommentById(
+            @PathVariable Long commentId, @Valid @RequestBody CommentDTO commentDTO) throws AccessDeniedException {
+        CommentDTO updatedComment = commentService.updateComment(commentId, commentDTO);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
     }
 
     @DeleteMapping("/comments/{commentId}")
