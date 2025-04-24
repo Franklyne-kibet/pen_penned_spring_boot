3289906f01e819af03ea81b0348060d2cb499a16
Franklyne-kibet <rootkibet@gmail.com>
2025-04-24 10:55:08 +0300

    feat: implement bookmark repo

--- src/main/java/com/pen_penned/blog/repositories/BookmarkRepository.java
+++ src/main/java/com/pen_penned/blog/repositories/BookmarkRepository.java
@@ -1,4 +1,26 @@
 package com.pen_penned.blog.repositories;
 
-public class BookmarkRepository {
+import com.pen_penned.blog.model.Bookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    /* // Find all bookmarks for a specific user with pagination
+    Page<Bookmark> findAllByUserId(Long id, Pageable pageDetails);
+
+    // Check if a bookmark exists for a specific user and post
+    boolean existsByUserIdAndPostId(Long id, Long postId); */
+
+    @Query("SELECT b FROM Bookmark b WHERE b.user.id = :userId")
+    Page<Bookmark> findBookmarksByUserId(@Param("userId") Long userId, Pageable pageDetails);
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Bookmark b WHERE b.user.id = :userId AND b.post.id = :postId")
+    boolean existsByUserIdAndPostId(@Param("userId") Long userId, @Param("postId") Long postId);
+    
 }
