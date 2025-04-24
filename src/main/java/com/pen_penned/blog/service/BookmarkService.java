9fc8a441f3aa9621f32b915717bf0c4f342e0db2
Franklyne-kibet <rootkibet@gmail.com>
2025-04-24 10:55:53 +0300

    feat: implement bookmark service

--- src/main/java/com/pen_penned/blog/service/BookmarkService.java
+++ src/main/java/com/pen_penned/blog/service/BookmarkService.java
@@ -1,4 +1,29 @@
 package com.pen_penned.blog.service;
 
-public class BookmarkService {
+import com.pen_penned.blog.dto.request.BookmarkDTO;
+import com.pen_penned.blog.dto.response.BookmarkResponse;
+import com.pen_penned.blog.dto.response.PageResponse;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+import java.nio.file.AccessDeniedException;
+
+public interface BookmarkService {
+
+    BookmarkResponse createBookMark(@Valid BookmarkDTO bookmarkDTO, Long userId);
+
+    @Transactional
+    void deleteBookmark(Long bookmarkId) throws AccessDeniedException;
+
+    PageResponse<BookmarkResponse> getUserBookmarks(
+            Long userId,
+            Integer pageNumber,
+            Integer pageSize,
+            String sortBy,
+            String sortOrder
+    );
+
+    BookmarkResponse getBookmark(Long bookmarkId) throws AccessDeniedException;
+
+    boolean isBookmarked(Long postId);
 }
