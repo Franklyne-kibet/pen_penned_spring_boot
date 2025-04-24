f3b1f7d7f8d9df1dd414f451c2446cbcbbd89ecb
Franklyne-kibet <rootkibet@gmail.com>
2025-04-24 10:53:07 +0300

    feat(bookmark): implement bookmarking dto req/res

--- src/main/java/com/pen_penned/blog/dto/request/BookmarkDTO.java
+++ src/main/java/com/pen_penned/blog/dto/request/BookmarkDTO.java
@@ -1,4 +1,11 @@
 package com.pen_penned.blog.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class BookmarkDTO {
+
+    @NotNull(message = "Post ID is required")
+    private Long postId;
 }
