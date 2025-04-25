e0d89b58ae399441cb514900805fe58204079460
Franklyne-kibet <rootkibet@gmail.com>
2025-04-25 17:44:45 +0300

    feat: dto requests

--- src/main/java/com/pen_penned/blog/dto/request/BookmarkFolderRequest.java
+++ src/main/java/com/pen_penned/blog/dto/request/BookmarkFolderRequest.java
@@ -1,4 +1,18 @@
 package com.pen_penned.blog.dto.request;
 
-public class BookmarkFolderRequest {
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+public record BookmarkFolderRequest(
+
+        @NotNull(message = "Bookmark ID cannot be null")
+        Long bookmarkId,
+
+        @NotNull(message = "Folder IDs cannot be null")
+        Set<Long> folderIds
+) {
+
 }
