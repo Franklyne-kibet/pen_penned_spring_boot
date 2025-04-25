13fa605b8c59f3b2aeb18820fec93c2db4fa7931
Franklyne-kibet <rootkibet@gmail.com>
2025-04-25 17:45:41 +0300

    feat: dto response

--- src/main/java/com/pen_penned/blog/dto/response/BookmarkResponse.java
+++ src/main/java/com/pen_penned/blog/dto/response/BookmarkResponse.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -16,5 +18,22 @@ public class BookmarkResponse {
     private Long id;
     private Long userId;
     private Long postId;
+    private String postTitle;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Set<FolderMinimalResponse> folders = new HashSet<>();
+
+
+    // Inner class for minimal folder information to avoid circular references
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class FolderMinimalResponse {
+
+        private Long id;
+        private String name;
+    }
+
 }
