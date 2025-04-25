3bc64848b4cd675fb9c28e1ede3e6f7f42b448b9
Franklyne-kibet <rootkibet@gmail.com>
2025-04-25 17:48:48 +0300

    feat: implement folder & bookmarkfolder

--- src/main/java/com/pen_penned/blog/model/BookmarkFolder.java
+++ src/main/java/com/pen_penned/blog/model/BookmarkFolder.java
@@ -1,4 +1,48 @@
 package com.pen_penned.blog.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table
 public class BookmarkFolder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_folder_id", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bookmark_id", nullable = false)
+    private Bookmark bookmark;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookmarkFolder that)) return false;
+        return getBookmark().equals(that.getBookmark()) &&
+                getFolder().equals(that.getFolder());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookmark(), getFolder());
+    }
 }
--- src/main/java/com/pen_penned/blog/model/Folder.java
+++ src/main/java/com/pen_penned/blog/model/Folder.java
@@ -1,4 +1,80 @@
 package com.pen_penned.blog.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(name = "folders",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_folder_user_name",
+                        columnNames = {"user_id", "name"}
+                )
+        },
+        indexes = {
+                @Index(
+                        name = "idx_folder_user",
+                        columnList = "user_id"
+                )
+        }
+)
 public class Folder {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folder_id", updatable = false)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    @Size(max = 255)
+    private String description;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<BookmarkFolder> bookmarkFolders = new HashSet<>();
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Folder folder)) return false;
+        return getName().equals(folder.getName()) &&
+                getUser().equals(folder.getUser());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getUser());
+    }
 }
