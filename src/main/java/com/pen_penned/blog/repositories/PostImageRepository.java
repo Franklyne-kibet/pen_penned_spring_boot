8f11a26ac2ef00dbc01d5b1843ad2dbeb939588e
Franklyne-kibet <rootkibet@gmail.com>
2025-05-02 11:06:35 +0300

    feat: implement post image repo

--- src/main/java/com/pen_penned/blog/repositories/PostImageRepository.java
+++ src/main/java/com/pen_penned/blog/repositories/PostImageRepository.java
@@ -1,4 +1,18 @@
 package com.pen_penned.blog.repositories;
 
-public class PostImageRepository {
+import com.pen_penned.blog.model.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+
+    List<PostImage> findByPostId(Long postId);
+
+    Optional<PostImage> findByS3Key(String s3Key);
+
+    void deleteByS3Key(String s3Key);
 }
